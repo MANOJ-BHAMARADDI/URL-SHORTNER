324b940e5ca604f802a9e872237a86f21c730d98
Manoj Bhamaraddi <manojbhamaraddi@gmail.com>
2025-03-14 23:26:54 +0530

    Fixed imports and start script

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ connectToMongoDB(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
+
 // Middleware
 app.use(express.json());
 app.use(cors());
